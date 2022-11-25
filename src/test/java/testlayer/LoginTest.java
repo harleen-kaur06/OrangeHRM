97ecd0d4b3f37f47937d0087dcbbde9790400d99
Harleen Kaur <harleenbhatia0605@gmailcom>
2022-11-26 03:34:43 +0530

    ExcelFile Integration

--- src/test/java/testlayer/LoginTest.java
+++ src/test/java/testlayer/LoginTest.java
@@ -3,9 +3,11 @@ package testlayer;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import Basepackage.BaseClass;
 import PomPackage.PomLogin;
+import testdata.ExcelSheet;
 
 public class LoginTest extends BaseClass {
 	PomLogin log;
@@ -34,6 +36,11 @@ public class LoginTest extends BaseClass {
 		log.clickbutton();
 	}
 	
+	@DataProvider 
+	public Object[][] Details(){ 
+		Object result[][]=ExcelSheet.readdata("Sheet1"); return result; 
+	}
+
 	@AfterMethod
 	public void close() {
 		if (driver != null) {
